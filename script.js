8faa5b85f1d003723d9fdc0492b20ac443638ed2
titusharma12 <sharmatitu119@gmail.com>
2023-12-30 17:47:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,291 @@
+function Listening() {
+  // alert('i enam listg')
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "flex";
+  Reputation.style.display = "none";
+  CRM.style.display = "none";
+  Analytics.style.display = "none";
+  Publishing.style.display = "none";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "none";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.add("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.remove("active");
+}
+function Reputation() {
+  // alert('repu')
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "flex";
+  CRM.style.display = "none";
+  Analytics.style.display = "none";
+  Publishing.style.display = "none";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "none";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.add("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.remove("active");
+}
+function CRM() {
+  // alert('crm')
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "none";
+  CRM.style.display = "flex";
+  Analytics.style.display = "none";
+  Publishing.style.display = "none";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "none";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.add("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.remove("active");
+}
+function Analytics() {
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "none";
+  CRM.style.display = "none";
+  Analytics.style.display = "flex";
+  Publishing.style.display = "none";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "none";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.add("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.remove("active");
+}
+function Publishing() {
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "none";
+  CRM.style.display = "none";
+  Analytics.style.display = "none";
+  Publishing.style.display = "flex";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "none";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.add("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.remove("active");
+}
+function Feedback() {
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "none";
+  CRM.style.display = "none";
+  Analytics.style.display = "none";
+  Publishing.style.display = "none";
+  Feedback.style.display = "flex";
+  Dashboards.style.display = "none";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.add("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.remove("active");
+}
+function Dashboards() {
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "none";
+  CRM.style.display = "none";
+  Analytics.style.display = "none";
+  Publishing.style.display = "none";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "flex";
+  Management.style.display = "none";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.add("active");
+  Crisis_Management.classList.remove("active");
+}
+function Management() {
+  let Listening = document.getElementById("Listening");
+  let Reputation = document.getElementById("Reputation");
+  let CRM = document.getElementById("CRM");
+  let Analytics = document.getElementById("Analytics");
+  let Publishing = document.getElementById("Publishing");
+  let Feedback = document.getElementById("Feedback");
+  let Dashboards = document.getElementById("Dashboards");
+  let Management = document.getElementById("Management");
+  Listening.style.display = "none";
+  Reputation.style.display = "none";
+  CRM.style.display = "none";
+  Analytics.style.display = "none";
+  Publishing.style.display = "none";
+  Feedback.style.display = "none";
+  Dashboards.style.display = "none";
+  Management.style.display = "flex";
+  let Social_Listening = document.getElementById("Social_Listening");
+  let Online_Reputation = document.getElementById("Online_Reputation");
+  let Social_CRM = document.getElementById("Social_CRM");
+  let Social_Analytics = document.getElementById("Social_Analytics");
+  let Social_Publishing = document.getElementById("Social_Publishing");
+  let Surveys_Feedback = document.getElementById("Surveys_Feedback");
+  let BI_Tools_Dashboards = document.getElementById("BI_Tools_Dashboards");
+  let Crisis_Management = document.getElementById("Crisis_Management");
+  Social_Listening.classList.remove("active");
+
+  // Remove the class from other buttons
+  Online_Reputation.classList.remove("active");
+  Social_CRM.classList.remove("active");
+  Social_Analytics.classList.remove("active");
+  Social_Publishing.classList.remove("active");
+  Surveys_Feedback.classList.remove("active");
+  BI_Tools_Dashboards.classList.remove("active");
+  Crisis_Management.classList.add("active");
+}
